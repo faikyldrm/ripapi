2bce7b108f82f7789ab032f788ac0a6a67e9b5ad
Cüneyt Güdek <cnt_gudek@hotmail.com>
2018-11-29 23:45:48 +0300

    first commit

--- ripapi/src/main/java/com/rest/RipapiApplication.java
+++ ripapi/src/main/java/com/rest/RipapiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RipapiApplication {
+public class RipapiApplication { //
 
 	public static void main(String[] args) {
 		SpringApplication.run(RipapiApplication.class, args);
